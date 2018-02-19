3b90dbcb83ab29b1e41d58c64103b6e867e9a37f
Pat Carlson <webdevpatricia@gmail.com>
2018-02-19 12:13:03 -0800

    Added code to server.js

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,44 @@
+var express = require("express") ;
+var burger = require("../models/burger.js");
+var router = express.Router();
+
+router.get("/", function(req, res) {
+  burger.all(function(data) {
+    var hbsObject = {
+      burgers: data
+    };
+    console.log(hbsObject);
+    res.render("index", hbsObject);
+  });
+});
+
+router.post("/api/burgers", function(req, res) {
+  burger.create(["name"], [req.body.name], function(result) {
+    // Send back the ID of the new quote
+    res.json({ id: result.insertId });
+  });
+});
+
+router.put("/api/burgers/:id", function(req, res) {
+  var condition = "id = " + req.params.id;
+
+  console.log("condition", condition);
+
+  burger.update(
+    {
+      name: req.body.name
+    },
+    condition,
+    function(result) {
+      if (result.changedRows === 0) {
+        // If no rows were changed, then the ID must not exist, so 404
+        return res.status(404).end();
+      }
+      res.status(200).end();
+
+    }
+  );
+});
+
+// Export routes for server.js to use.
+module.exports = router;
--- server.js
+++ server.js
@@ -1,5 +1,34 @@
-var express require("express");
-var methodOverride require("method-override");
-var bodyParser require("body-parser");
+
+//require packages
+var express = require("express");
+var methodOverride  = require("method-override");
+var bodyParser  = require("body-parser");
+var expressHandlebars = require("express-handlebars");
+var routes = require("./controllers/burgers_controller.js");
+
+//assign a port and create the express app
+var PORT = process.env.PORT || 8080;
+var app = express();
+
+app.use(express.static("public"));
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(bodyParser.json());
+
+//set Handlebars
+app.engine("handlebars", expressHandlebars({
+	defaultLayout: "main"}));
+app.set("view engine", "handlebars");
+
+// implement routes
+app.use(routes);
+
+app.listen(PORT, function(){
+	console.log("App now listening at localhost: " + PORT);
+});
+
+
 
 
