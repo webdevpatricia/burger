de4cd17128bb8246269519d9ff3d54663b918ac4
Pat Carlson <webdevpatricia@gmail.com>
2018-02-19 13:29:50 -0800

    Adding connection to jaws

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,17 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
+// Make connection.
+if (process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  var connection = mysql.createConnection({
   port: 8080,
   host: "localhost",
   user: "root",
   password: "Iamnumber1",
   database: "burgers_db"
 });
-
-// Make connection.
+};
 connection.connect(function(err) {
   if (err) {
     console.error("error connecting: " + err.stack);
