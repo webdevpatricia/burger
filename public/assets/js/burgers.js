74a7816c8022c9f920bdd99d16d505b3ceb67a15
Pat Carlson <webdevpatricia@gmail.com>
2018-02-21 17:31:39 -0800

    final push

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,22 +1,24 @@
 
+$(function() {
+	$(".create-form").on("submit", function(event) {
+		var id = $(this).data("id");
+	    event.preventDefault();
 
-$(".create-form").on("submit", function(event) {
-    event.preventDefault();
+	    var newBurger = {
+	      name: $("#burger").val().trim()
+	    };
+	    console.log(newBurger);
 
-    var newBurger = {
-      name: $("#burger").val().trim(),
-      
-    };
-
-    // Send the POST request.
-    $.ajax("/api/burgers", {
-      type: "POST",
-      data: newBurger
-    }).then(
-      function() {
-        console.log("created new burger");
-        // Reload the page to get the updated list
-        location.reload();
-      }
-    );
-  });
+	    // Send the POST request.
+	    $.ajax("/api/burgers", {
+	      type: "POST",
+	      data: newBurger
+	    }).then(
+	      function() {
+	        console.log("created new burger");
+	        // Reload the page to get the updated list
+	        location.reload();
+	      }
+	    );
+	});
+});
